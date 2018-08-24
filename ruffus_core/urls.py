b805370e2dcb1755b29621a45e69f5d78fc24d29
doyleSM <gbalk@inf.ufsm.br>
2018-08-23 21:33:53 -0300

    criacao da app para a index e esboço da tela inicial

--- ruffus_core/urls.py
+++ ruffus_core/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from csite.views import index
 urlpatterns = [
+    path('', index),
     path('admin/', admin.site.urls),
 ]
