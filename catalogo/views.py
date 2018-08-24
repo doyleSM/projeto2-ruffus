7a1a485686e9364a8f7abb14d9191c3aa614204c
doyleSM <gbalk@inf.ufsm.br>
2018-08-24 20:57:47 -0300

    mudando de funcao class based view na app catalogo

--- catalogo/views.py
+++ catalogo/views.py
@@ -1,29 +1,41 @@
-from django.shortcuts import render
-
+from django.shortcuts import get_object_or_404
+from django.views import generic
 # Create your views here.
 from .models import Servico, Categoria
 
+class ListaServicosView(generic.ListView):
+    model = Servico
+    template_name = 'catalogo/lista_servicos.html'
+    context_object_name = 'servicos'
 
-def lista_servicos(request):
-    contexto = {
-        'servicos': Servico.objects.all()
-    }
-    return render(request, 'catalogo/lista_servicos.html', contexto)
+lista_servicos = ListaServicosView.as_view()
 
 
-def lista_por_categoria(request, slug):
 
-    categoria = Categoria.objects.get(slug=slug)
-    contexto = {
-        'categoria_atual': categoria,
-        'servicos': Servico.objects.filter(categoria=categoria)
-    }
-    return render(request, 'catalogo/servicos_categoria.html', contexto)
+class ListaPorCategoriaView(generic.ListView):
 
-def detalhe_servico(request, slug):
+    template_name = 'catalogo/servicos_categoria.html'
+    context_object_name = 'servicos'
 
-    servico = Servico.objects.get(slug=slug)
-    contexto = {
-        'servico': servico
-    }
-    return render(request, 'catalogo/detalhe_servico.html', contexto)
+    def get_queryset(self):
+        categoria = get_object_or_404(Categoria, slug=self.kwargs['slug'])
+        return Servico.objects.filter(categoria=categoria)
+
+    def get_context_data(self, **kwargs):
+        context = super(ListaPorCategoriaView, self).get_context_data(**kwargs)
+        context['categoria_atual'] = get_object_or_404(Categoria, slug=self.kwargs['slug'])
+        return context
+
+lista_por_categoria = ListaPorCategoriaView.as_view()
+
+
+class DetalheServicoView(generic.ListView):
+
+    template_name = 'catalogo/detalhe_servico.html'
+    context_object_name = 'servico'
+
+    def get_queryset(self):
+        return Servico.objects.get(slug=self.kwargs['slug'])
+
+
+detalhe_servico = DetalheServicoView.as_view()
