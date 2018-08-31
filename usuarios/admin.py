65e8259a6ef06cd744fe3ef3e39111f5c11d7321
doyleSM <gbalk@inf.ufsm.br>
2018-08-31 12:46:19 -0300

    ajustes tela gerencia admin

--- usuarios/admin.py
+++ usuarios/admin.py
@@ -2,8 +2,9 @@ from django.contrib import admin
 from .models import Cliente, User, Endereco
 
 class ClienteAdmin(admin.ModelAdmin):
-    list_display = ['nome', 'usuario', 'CPF', 'status', 'endereco']
-    #search_fields = ['nome', 'slug']
+    fields = [('CPF', 'user'), ]
+    list_display = ['usuario', 'nome', 'CPF', 'status', 'endereco']
+    search_fields = ['nome',]
 
     def nome(self, obj):
         return obj.user.first_name + ' ' + obj.user.last_name
@@ -21,8 +22,16 @@ class ClienteAdmin(admin.ModelAdmin):
         endereco = Endereco.objects.get(usuario=usuario)
         return endereco
 
+class EnderecoAdmin(admin.ModelAdmin):
+    list_display = ['nome_rua', 'complemento', 'bairro', 'CEP', 'usuario']
+    search_fields = ['nome_rua',]
+    autocomplete_fields = ('usuario',)
+    
+class UserAdmin(admin.ModelAdmin):
+    search_fields = ['nome',]
+
 
 # Register your models here.
 admin.site.register(Cliente, ClienteAdmin)
-admin.site.register(User)
-admin.site.register(Endereco)
+admin.site.register(User, UserAdmin)
+admin.site.register(Endereco, EnderecoAdmin)
