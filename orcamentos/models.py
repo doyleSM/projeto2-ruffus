a4c7fccd45ae6333ed888730a496e1fa19b6959c
doyleSM <gbalk@inf.ufsm.br>
2018-09-04 14:11:18 -0300

    model solicitacao

--- orcamentos/models.py
+++ orcamentos/models.py
@@ -1,3 +1,21 @@
 from django.db import models
-
+from usuarios.models import Cliente, Endereco
+from catalogo.models import Servico
 # Create your models here.
+
+class Solicitacao(models.Model):
+    cliente = models.OneToOneField(Cliente, on_delete=models.PROTECT)
+    servico = models.OneToOneField(Servico, on_delete=models.PROTECT)
+    aberta = models.BooleanField('Status', default=True)
+    endereco = models.OneToOneField(Endereco, on_delete=models.PROTECT)
+    descricao = models.TextField('Descricao')
+    hora_solicitacao = models.DateTimeField(auto_now_add=True)
+    hora_aceitacao = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return 'Solicitacao teste'
+
+    class Meta:
+        verbose_name_plural = 'Solicitações'
+        verbose_name = 'Solicitação'
+
--- usuarios/models.py
+++ usuarios/models.py
@@ -2,11 +2,15 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 from catalogo.models import Categoria
 
+
 class User(AbstractUser):
+
     is_cliente = models.BooleanField(default=False)
     is_prestador = models.BooleanField(default=False)
 
+
 class Endereco(models.Model):
+
     nome_rua = models.CharField('Rua', max_length=50 )
     complemento = models.CharField('Complemento', max_length=30, null=True, blank=True)
     bairro = models.CharField('Bairro', max_length=50)
@@ -18,6 +22,7 @@ class Endereco(models.Model):
 
 
 class Cliente(models.Model):
+
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
     CPF = models.CharField('CPF', max_length=15, default='123456789')
 
@@ -26,6 +31,7 @@ class Cliente(models.Model):
         return self.user.username
 
 class Prestador(models.Model):
+
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
     CPF = models.CharField('CPF', max_length=15, default='12345678')
     categorias = models.ManyToManyField(Categoria, blank=True)
