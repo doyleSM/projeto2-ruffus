d639060db540b2e90e3c2f9dbd389891872dbe59
doyleSM <gbalk@inf.ufsm.br>
2018-09-23 12:14:25 -0300

    view visualização perfil prest

--- perfis/views.py
+++ perfis/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from usuarios.models import Prestador
+from django.views.generic import ListView
 
 # Create your views here.
+
+class PerfilPrestador(ListView):
+
+    template_name = 'perfis/perfil_prestador'
+    context_object_name = 'perfil'
+
+    #paginate_by = 5
+
+    def get_queryset(self):
+        return Prestador.objects.get(self.kwargs['pk'])
